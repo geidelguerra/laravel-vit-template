f15fcb964b460531e2923cdb8f49c95cd365c0a0
Geidel Guerra <geidelguerra@protonmail.com>
2022-09-20 18:30:52 -0400

    Add commented setup for Laravel Echo when using Inertia

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,13 @@ import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/inertia-vue3'
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
 
+// Uncomment this  if you are using Laravel Echo
+// import { Inertia } from '@inertiajs/inertia'
+
+// Inertia.on('before', (event) => {
+//   event.detail.visit.headers['X-Socket-ID'] = window.Echo.socketId()
+// })
+
 createInertiaApp({
   resolve: name => resolvePageComponent(`./pages/${name}.vue`, import.meta.glob('./pages/**/*.vue')),
   setup({ el, App, props, plugin }) {
